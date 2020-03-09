11dabff5819e07e61a38850a67f66d28b25a3d73
Paul Sinnett <paul.sinnett@gmail.com>
2020-03-09 12:33:00 +0000

    additional steps

---
+++ Count.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Count
+{
+    static void Main(string[] args)
+    {
+        int count = 1;
+    loop:
+        Console.WriteLine(count);
+        if (count == 10) goto end;
+        count = count + 1;
+        goto loop;
+    end:
+        return;
+    }
+}
